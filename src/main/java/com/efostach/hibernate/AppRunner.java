e7b08491c3651fbc5777fb8c71032fac3e798e73
Helen Fostach <elena.fostach@gmail.com>
2019-12-17 13:32:26 +0200

    added configuration files

---
+++ src/main/java/com/efostach/hibernate/AppRunner.java
@@ -0,0 +1,12 @@
+package com.efostach.hibernate;
+
+import com.efostach.hibernate.view.AppConsole;
+
+public class AppRunner {
+
+    public static void main(String[] args) {
+
+        AppConsole ac = new AppConsole();
+        ac.run();
+    }
+}
